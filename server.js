afb4c4839e87301b5520e667f102647e740a762d
Om Salunkhe <omsalunkhe1441@gmail.com>
2026-02-14 14:35:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+//serrver ko start krna
+//database se connect krna
+
+require("dotenv").config()
+const connectToDb = require("./src/config/database")
+const app = require("./src/app")
+
+
+
+//connecting to mongodb
+connectToDb()
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
---
+++ src/app.js
@@ -0,0 +1,31 @@
+//server ko create krna
+//server ko config krna
+
+const express = require("express")
+const noteModel = require("./models/notes.model")
+const app = express()
+
+
+
+app.use(express.json());
+
+
+/**
+ * -POST /notes
+ * req.body =>{title,description}
+ */
+app.post("/notes", async (req,res)=>{
+    const {title,description }= req.body
+    const note = await noteModel.create({
+        title , description
+    })
+
+    res.status(201).json({
+        message:"note created successfully",
+        note
+    })
+})
+
+
+
+module.exports= app
